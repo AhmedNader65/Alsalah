dbea73e7dd907b4a92df350def4445feae42beb5
AhmedNader <ahmed.nader1994@gmail.com>
2022-04-26 12:44:16 +0200

    change dashboard to azkar

--- app/src/main/java/com/crazyidea/alsalah/ui/azkar/AzkarFragment.kt
+++ app/src/main/java/com/crazyidea/alsalah/ui/azkar/AzkarFragment.kt
@@ -1,4 +1,4 @@
-package com.crazyidea.alsalah.ui.dashboard
+package com.crazyidea.alsalah.ui.azkar
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -9,7 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.crazyidea.alsalah.databinding.FragmentDashboardBinding
 
-class DashboardFragment : Fragment() {
+class AzkarFragment : Fragment() {
 
     private var _binding: FragmentDashboardBinding? = null
 
@@ -23,7 +23,7 @@ class DashboardFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+            ViewModelProvider(this).get(AzkarViewModel::class.java)
 
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
