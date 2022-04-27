a74cd37418a7bad726cd2ccc66f92cdd5dd32247
abdelrahim <abdelrahimgamal97@gmail.com>
2022-04-27 13:50:17 +0200

    menu page

---
+++ app/src/main/java/com/crazyidea/alsalah/ui/menu/MenuFragment.kt
@@ -0,0 +1,46 @@
+package com.crazyidea.alsalah.ui.menu
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.crazyidea.alsalah.R
+import com.crazyidea.alsalah.databinding.FragmentMenuBinding
+
+class MenuFragment : Fragment() {
+
+    private var _binding: FragmentMenuBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val menuViewModel =
+            ViewModelProvider(this).get(MenuViewModel::class.java)
+
+        _binding = FragmentMenuBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.setting.setOnClickListener {
+            findNavController().navigate(R.id.settingFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
