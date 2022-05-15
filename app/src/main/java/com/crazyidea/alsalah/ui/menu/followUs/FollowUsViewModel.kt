48257bbc15a1de2727248d32d0b696d62f341fbf
abdelrahimgamal <76844212+abdelrahimgamal@users.noreply.github.com>
2022-05-15 14:05:23 +0200

    follow us

---
+++ app/src/main/java/com/crazyidea/alsalah/ui/menu/followUs/FollowUsViewModel.kt
@@ -0,0 +1,10 @@
+package com.crazyidea.alsalah.ui.menu.followUs
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class FollowUsViewModel : ViewModel() {
+
+
+}
