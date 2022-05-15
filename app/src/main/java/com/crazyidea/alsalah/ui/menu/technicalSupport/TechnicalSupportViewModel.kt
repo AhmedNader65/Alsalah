c4717107a40fec066a92f306a09798227ae508df
abdelrahimgamal <76844212+abdelrahimgamal@users.noreply.github.com>
2022-05-15 14:19:03 +0200

    connecting fragments

--- app/src/main/java/com/crazyidea/alsalah/ui/menu/technicalSupport/TechnicalSupportViewModel.kt
+++ app/src/main/java/com/crazyidea/alsalah/ui/menu/technicalSupport/TechnicalSupportViewModel.kt
@@ -3,7 +3,6 @@ package com.crazyidea.alsalah.ui.menu.technicalSupport
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 
-@HiltViewModel
 class TechnicalSupportViewModel : ViewModel() {
 
 
--- app/src/main/java/com/crazyidea/alsalah/ui/menu/whoAreWe/WhoAreWeViewModel.kt
+++ app/src/main/java/com/crazyidea/alsalah/ui/menu/whoAreWe/WhoAreWeViewModel.kt
@@ -3,7 +3,6 @@ package com.crazyidea.alsalah.ui.menu.whoAreWe
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 
-@HiltViewModel
 class WhoAreWeViewModel : ViewModel() {
 
 
