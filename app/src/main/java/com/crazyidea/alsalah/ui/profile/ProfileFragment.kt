21cc90b432d287110ab2b522bfd6d8b330357481
abdelrahimgamal <76844212+abdelrahimgamal@users.noreply.github.com>
2022-05-24 13:13:12 +0200

    night mode switch

--- app/src/main/java/com/crazyidea/alsalah/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/crazyidea/alsalah/ui/profile/ProfileFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
@@ -36,6 +37,9 @@ class ProfileFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES)
+            binding.nightModeSwitch.isChecked = true else false
         binding.blueColor.setOnClickListener {
             setSelectedSecondaryColor(binding.selectedBlue)
         }
@@ -45,6 +49,16 @@ class ProfileFragment : Fragment() {
         binding.pinkColor.setOnClickListener {
             setSelectedSecondaryColor(binding.selectedPink)
         }
+
+        binding.nightModeSwitch.setOnCheckedChangeListener { view, isChecked ->
+            if (isChecked)
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            else
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+        }
+
+
     }
 
     override fun onDestroyView() {
