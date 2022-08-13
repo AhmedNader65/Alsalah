d2326f94c7cec9d6abbe8e62a5aaef5086de6900
Ahmed <ahmed.nader1994@gmail.com>
2022-08-13 14:39:47 +0200

    merge

--- app/src/main/java/com/crazyidea/alsalah/ui/setting/Settings.kt
+++ app/src/main/java/com/crazyidea/alsalah/ui/setting/Settings.kt
@@ -1,8 +1,10 @@
 package com.crazyidea.alsalah.ui.setting
 
+import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.datastore.preferences.core.stringPreferencesKey
 
 object AppSettings{
     val APP_LANGUAGE = stringPreferencesKey("app-language")
+    val aAPP_LANGUAGE = booleanPreferencesKey("app-language")
 }
